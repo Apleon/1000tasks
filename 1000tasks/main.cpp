1b72e65a3cecfffd1061d096bcfeec625345d740
Apleon <shara49750@gmail.com>
2021-11-17 20:06:53 +0300

    Initial Commit

---
+++ 1000tasks/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1000tasks
+//
+//  Created by Андрей Шарапов on 17.11.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
